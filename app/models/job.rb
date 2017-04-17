67e9cc11de2fe01fc97c23f112a09a1c59f31f1f
jonathan <zhanggongzifei@163.com>
2017-04-18 00:27:36 +0800

    order by wage

--- app/models/job.rb
+++ app/models/job.rb
@@ -5,6 +5,7 @@ class Job < ApplicationRecord
   validates :wage_lower_bound, numericality: { greater_than: 3000}
 
   scope :recent, -> { order("created_at DESC")}
+  scope :published, -> { where(is_hidden: false)}
 
   def publish!
    self.is_hidden = false
@@ -15,5 +16,7 @@ class Job < ApplicationRecord
    self.is_hidden = true
    self.save
  end
+
+
 
 end
