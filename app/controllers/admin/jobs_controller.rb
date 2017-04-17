02bba944e977196d9bbf49461f62e38efbe4ed69
jonathan <zhanggongzifei@163.com>
2017-04-17 23:34:38 +0800

    add more details to job

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -44,7 +44,7 @@ end
 private
 
 def job_params
-  params.require(:job).permit(:title, :description)
+  params.require(:job).permit(:title, :description, :wage_lower_bound, :wage_upper_bound, :contact_email, :is_hidden)
 end
 
 end
