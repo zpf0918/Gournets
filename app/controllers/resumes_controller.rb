b7dc7567ba0c8559ebf3a2307cf7d852d5ec3b1e
jonathan <zhanggongzifei@163.com>
2017-04-18 01:28:12 +0800

    install carrierwave

--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -22,6 +22,6 @@ class ResumesController < ApplicationController
   private
 
   def resume_params
-    params.require(:resume).permit(:content)
+    params.require(:resume).permit(:content, :attachment)
   end
 end
