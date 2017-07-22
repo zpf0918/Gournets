3cffcd578fa253ed5bdd7cfc4fa28e4f4a8b0bde
zpf0918 <zhanggongzifei@163.com>
2017-07-22 22:24:10 +0800

    better job_controller.rb code

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,103 +1,104 @@
 class JobsController < ApplicationController
-before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy, :favorite]
-before_action :validate_search_key, only: [:search]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy, :favorite]
+  before_action :validate_search_key, only: [:search]
 
-def favorite
-   @job = Job.find(params[:id])
-    type = params[:type]
-   if type == "favorite"
-     current_user.favorite_jobs << @job
-     redirect_to :back
 
-  elsif type == "unfavorite"
-     current_user.favorite_jobs.delete(@job)
-    redirect_to :back
 
-  else
-    redirect_to :back
+  def index
+    @jobs = case params[:order]
+          when 'by_lower_bound'
+            Job.published.order("wage_lower_bound DESC").paginate(:page => params[:page], :per_page => 5)
+          when  'by_upper_bound'
+            Job.published.order("wage_upper_bound DESC").paginate(:page => params[:page], :per_page => 5)
+          else
+            Job.published.recent.paginate(:page => params[:page], :per_page => 5)
+          end
   end
-end
 
-def index
-  @jobs = case params[:order]
-        when 'by_lower_bound'
-          Job.published.order("wage_lower_bound DESC").paginate(:page => params[:page], :per_page => 5)
-        when  'by_upper_bound'
-          Job.published.order("wage_upper_bound DESC").paginate(:page => params[:page], :per_page => 5)
-        else
-          Job.published.recent.paginate(:page => params[:page], :per_page => 5)
-        end
-end
+  def show
+    @job = Job.find(params[:id])
 
-def show
-  @job = Job.find(params[:id])
-
-  if @job.is_hidden
-    flash[:warning] = "该工作已被隐藏"
-    redirect_to root_path
+    if @job.is_hidden
+      flash[:warning] = "该工作已被隐藏"
+      redirect_to root_path
+    end
   end
-end
 
-def new
-  @job = Job.new
-end
+  def new
+    @job = Job.new
+  end
 
-def create
-  @job = Job.new(job_params)
-  @job.user = current_user
-  if @job.save
+  def create
+    @job = Job.new(job_params)
+    @job.user = current_user
+    if @job.save
+      redirect_to jobs_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @job = Job.find(params[:id])
+  end
+
+  def update
+    @job = Job.find(params[:id])
+    if @job.update(job_params)
+      redirect_to jobs_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @job = Job.find(params[:id])
+    @job.destroy
     redirect_to jobs_path
-  else
-    render :new
   end
-end
 
-def edit
-  @job = Job.find(params[:id])
-end
+  def favorite
+     @job = Job.find(params[:id])
+      type = params[:type]
+     if type == "favorite"
+       current_user.favorite_jobs << @job
+       redirect_to :back
 
-def update
-  @job = Job.find(params[:id])
-  if @job.update(job_params)
-    redirect_to jobs_path
-  else
-    render :edit
+    elsif type == "unfavorite"
+       current_user.favorite_jobs.delete(@job)
+      redirect_to :back
+
+    else
+      redirect_to :back
+    end
   end
-end
 
-def destroy
-  @job = Job.find(params[:id])
-  @job.destroy
-  redirect_to jobs_path
-end
-
-def search
-  if @query_string.present?
-    search_result = Job.published.ransack(@search_criteria).result(:distinct => true)
-    @jobs = search_result.recent.paginate(:page => params[:page], :per_page => 5 )
+  def search
+    if @query_string.present?
+      search_result = Job.published.ransack(@search_criteria).result(:distinct => true)
+      @jobs = search_result.recent.paginate(:page => params[:page], :per_page => 5 )
+    end
   end
-end
 
-protected
-def validate_search_key
-  @query_string = params[:q].gsub(/\\|\'|\/|\?/, "")
-  if params[:q].present?
-    @search_criteria =  {
-      title_or_company_or_city_cont: @query_string
-    }
+  protected
+
+  def validate_search_key
+    @query_string = params[:q].gsub(/\\|\'|\/|\?/, "")
+    if params[:q].present?
+      @search_criteria =  {
+        title_or_company_or_city_cont: @query_string
+      }
+    end
   end
-end
 
-def search_criteria(query_string)
-  { :title_cont => query_string }
-end
+  def search_criteria(query_string)
+    { :title_cont => query_string }
+  end
 
-private
+  private
 
-def job_params
-   params.require(:job).permit(:title,:description,:wage_upper_bound,:wage_lower_bound,:contact_email, :is_hidden, :category, :company, :city)
-end
-
-
+  def job_params
+     params.require(:job).permit(:title,:description,:wage_upper_bound,:wage_lower_bound,:contact_email, :is_hidden, :category, :company, :city)
+  end
 
 end
